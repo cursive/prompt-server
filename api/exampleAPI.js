88617a151ca364c0686dcb90efe047513e71cebc
Daniel Nacamuli <reg@cursive.co.uk>
2023-05-15 14:45:06 -0700

    Changed server log

--- api/exampleAPI.js
+++ api/exampleAPI.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Define API routes
 router.post('/message', (req, res) => {
     console.log("Server in message")
-    const message = 'This is a simple API message.';
+    const message = 'This is a simple API message I jusrt updated.';
     res.json({ message });
 });
 
