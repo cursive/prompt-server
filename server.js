9bced7fb8c7bb9d34dd4f0e8b7e9baa8e4de7d8d
Daniel Nacamuli <reg@cursive.co.uk>
2023-05-15 15:14:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,17 @@ dotenv.config({ path: './.env' });
 import exampleAPI from './api/exampleAPI.js';
 import promptAPI from './api/promptAPI.js';
 
-
-if (process.env.NODE_ENV === 'replit') {
-    // Replit environment, access secrets directly
-    console.log("Running on replit, ehre is the API key", process.env.OPENAI_API_KEY);
-} else {
-    // Local development environment, load .env file
-    dotenv.config({ path: './.env' });
-    console.log("Running on replit, ehre is the API key", process.env.OPENAI_API_KEY);
-}
-
-
 const app = express();
 const port = 3000;
+
+// Check environment and configure environment variables accordingly
+if (process.env.NODE_ENV === 'replit') {
+    // Replit environment, access secrets directly
+    console.log("Running on replit, here is the API key", process.env.OPENAI_API_KEY);
+} else {
+    // Local development environment
+    console.log("Running locally, here is the API key", process.env.OPENAI_API_KEY);
+}
 
 // Serve static files from the "static" directory
 app.use(express.static('static'));
